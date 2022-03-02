23550e8a9eb5d708864df960243b6d27563a6848
Hessam Imen <hessam.imen@gmail.com>
2022-03-02 23:03:32 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'x';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'y';
+}
